e7d888322b9ccf49408772014ca5a45226e60019
prashant169 <prashantphad2018@gmail.com>
2021-10-26 03:22:11 -0800

    Added:Program dor the email validation test parametarised test

--- UserRegistrationLambda/src/main/java/com/bridgelabz/UserValidation.java
+++ UserRegistrationLambda/src/main/java/com/bridgelabz/UserValidation.java
@@ -4,38 +4,75 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidation {
-	public static boolean firstName(String firstName) {
-		String regex = "^[A-Z]{1}[a-z]{2,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(firstName);
-		return matcher.matches();
-	}
+    public static boolean firstNameValidation(String firstName) throws UserValidationException {
+        try {
+            UserValidate<String> obj = (fn) -> {
+                String regex = "^[A-Z]{1}[a-z]{2,}$";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(fn);
+                return matcher.matches();
+            };
+            return obj.validate(firstName);
 
-	public static boolean lastName(String lastName) {
-		String regex = "^[a-zA-Z]*$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(lastName);
-		return matcher.matches();
-	}
+        } catch (NullPointerException exception) {
+            throw new UserValidationException("Invalid input");
+        }
+    }
 
-	public static boolean email(String email) {
-		String regex = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z0-9]+.(com|net)(.[a-z]{2,3}){0,1}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(email);
-		return matcher.matches();
-	}
+    public static boolean lastNameValidation(String lastName) throws UserValidationException {
+        try {
+            UserValidate<String> obj = (ln) -> {
+                String regex = "^[a-zA-Z]*$";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(ln);
+                return matcher.matches();
+            };
+            return obj.validate(lastName);
 
-	public static boolean phoneNumber(String phoneNumber) {
-		String regex = "^(?:(?:\\+|0{0,2})91(\\s*[\\-]\\s*)?|[0]?)?[789]\\d{9}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(phoneNumber);
-		return matcher.matches();
-	}
+        } catch (NullPointerException exception) {
+            throw new UserValidationException("Invalid input");
+        }
+    }
 
-	public static boolean password(String password) {
-		String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(password);
-		return matcher.matches();
-	}
-}
+    public static boolean emailValidation(String email) throws UserValidationException {
+        try {
+            UserValidate<String> obj = (em) -> {
+                String regex = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z0-9]+.(com|net)(.[a-z]{2,3}){0,1}$";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(em);
+                return matcher.matches();
+            };
+            return obj.validate(email);
+        } catch (NullPointerException exception) {
+            throw new UserValidationException("Invalid input");
+        }
+    }
+
+    public static boolean phoneNumberValidation(String phoneNumber) throws UserValidationException {
+        try {
+            UserValidate<String> obj = (pn) -> {
+                String regex = "^(?:(?:\\+|0{0,2})91(\\s*[\\-]\\s*)?|[0]?)?[789]\\d{9}$";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(pn);
+                return matcher.matches();
+            };
+            return obj.validate(phoneNumber);
+        } catch (NullPointerException exception) {
+            throw new UserValidationException("Invalid input");
+        }
+    }
+
+    public static boolean passwordValidation(String password) throws UserValidationException {
+        try {
+            UserValidate<String> obj = (ps) -> {
+                String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(ps);
+                return matcher.matches();
+            };
+            return obj.validate(password);
+        } catch (NullPointerException exception) {
+            throw new UserValidationException("Invalid input");
+        }
+    }
+}}
--- UserRegistrationLambda/src/test/java/com/bridgelabz/UserValidationTest.java
+++ UserRegistrationLambda/src/test/java/com/bridgelabz/UserValidationTest.java
@@ -4,58 +4,58 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserValidationTest {
-    @Test
-    public void givenFirstName_Validation() {
-        UserValidation validation = new UserValidation();
-        boolean actualResult = true;
-        try {
-            actualResult = validation.firstName("prashant");
-        } catch (UserValidationException exception) {
-            Assert.assertEquals(actualResult, exception.message);
-        }
-    }
+	@Test
+	public void givenFirstName_Validation() {
+		UserValidation validation = new UserValidation();
+		boolean actualResult = true;
+		try {
+			actualResult = validation.firstNameValidation("prashant");
+		} catch (UserValidationException exception) {
+			Assert.assertEquals(actualResult, exception.message);
+		}
+	}
 
-    @Test
-    public void lastName_Validation() {
-        UserValidation validation = new UserValidation();
-        boolean actualResult = true;
-        try {
-            actualResult = validation.lastName("phad");
-        } catch (UserValidationException exception) {
-            Assert.assertEquals(actualResult, exception.message);
-        }
-    }
+	@Test
+	public void giveLastName_Validation() {
+		UserValidation validation = new UserValidation();
+		boolean actualResult = true;
+		try {
+			actualResult = validation.lastNameValidation("prashant");
+		} catch (UserValidationException exception) {
+			Assert.assertEquals(actualResult, exception.message);
+		}
+	}
 
-    @Test
-    public void email_Validation() {
-        UserValidation validation = new UserValidation();
-        boolean actualResult = true;
-        try {
-            actualResult = validation.email("prashant007@gmail.com");
-        } catch (UserValidationException exception) {
-            Assert.assertEquals(actualResult, exception);
-        }
-    }
+	@Test
+	public void givenEmail_Validation() {
+		UserValidation validation = new UserValidation();
+		boolean actualResult = true;
+		try {
+			actualResult = validation.emailValidation("mkaubr007@gmail.com");
+		} catch (UserValidationException exception) {
+			Assert.assertEquals(actualResult, exception);
+		}
+	}
 
-    @Test
-    public void phoneNumber_Validation() {
-        UserValidation validation = new UserValidation();
-        boolean actualResult = true;
-        try {
-            actualResult = validation.phoneNumber("+918002695700");
-        } catch (UserValidationException exception) {
-            Assert.assertEquals(actualResult, exception.message);
-        }
-    }
+	@Test
+	public void givenPhoneNumber_Validation() {
+		UserValidation validation = new UserValidation();
+		boolean actualResult = true;
+		try {
+			actualResult = validation.phoneNumberValidation("+919970899057");
+		} catch (UserValidationException exception) {
+			Assert.assertEquals(actualResult, exception.message);
+		}
+	}
 
-    @Test
-    public void password_Validation() {
-        UserValidation validation = new UserValidation();
-        boolean actualResult=true;
-        try {
-            actualResult = validation.password("qw1rt@Ms");
-        }catch (UserValidationException exception) {
-            Assert.assertEquals(actualResult,exception.message);
-        }
-    }
+	@Test
+	public void givenPassword_Validation() {
+		UserValidation validation = new UserValidation();
+		boolean actualResult = true;
+		try {
+			actualResult = validation.passwordValidation("qw1rt@Ms");
+		} catch (UserValidationException exception) {
+			Assert.assertEquals(actualResult, exception.message);
+		}
+	}
 }
